3772e981e9c2d38a305c467bfc69446be20454fd
Kuuwang <kuuwang@gmail.com>
2024-09-24 00:12:27 +0900

    [시뮬레이터] 불필요 콘솔로그 제거

--- simulator/js/job.js
+++ simulator/js/job.js
@@ -19,7 +19,6 @@ function updateJob(image) {
     const jobname = image.alt;
     const jobIdString = image.dataset.db; 
     const jobIds = JOBID[jobIdString]; 
-    console.log(jobIds)
     
     // 직업 계층 구조 가져오기
     const JobInherit = getJobInherit(jobIds);
@@ -34,7 +33,6 @@ function updateJob(image) {
     // 직업 계층에 따라 모든 스킬 ID를 가져오는 로직
     JobInherit.forEach(job => {
         const skills = SKILL_TREEVIEW_FOR_JOB[job];
-        console.log(Object.keys(skills))
         if (skills) {
             for (const skillId in skills) {
                 skillIds.push(skills[skillId]); // 스킬 ID를 배열에 추가
@@ -42,5 +40,5 @@ function updateJob(image) {
         }
     });
     // 스킬 트리 표시
-    createSkillGrid(JobInherit, skillIds); // 스킬 그리드 생성 함수 호출
+    createSkillGrid(JobInherit); // 스킬 그리드 생성 함수 호출
 }
