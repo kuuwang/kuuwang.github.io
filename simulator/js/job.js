d4b1b8e7722df24ffdb695bf96b94e2310614934
Kuuwang <kuuwang@gmail.com>
2024-09-27 00:43:03 +0900

    [시뮬레이터] 골치가 아픔

--- simulator/js/job.js
+++ simulator/js/job.js
@@ -31,7 +31,7 @@ function updateJob(image) {
     charBaseLV.value = charValue[0];
     const charJobLV = document.getElementById("jobLV");
     charJobLV.value = charValue[1];
-    
+    const skillIds = [];
     JobInherit.forEach(job => {
       const skills = SKILL_TREEVIEW_FOR_JOB[job];
         if (skills) {
